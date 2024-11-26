ea44cd374714ec37dbcd0e506d19caa0ff314b8e
Plopkoekk <41813101+perquyk@users.noreply.github.com>
2024-11-26 17:41:36 +0100

    did excercise 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    
+    items.forEach(item => 
+        { 
+            array = array.filter((value) => value !== item);
+            return array;
+        });
+
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(...numbers) {
+    let num = numbers.sort((a,b) => a - b); 
+    let end = num[1];
+    let start = num[0];
+    let outcome = 0;
+    if(num[0] < 0){return "ERROR"}
+    if( numbers[1] !== parseInt(numbers[1]) || numbers[0]  !== parseInt(numbers[0] )){ return "ERROR"}
+    
+    while(start <= end){
+        outcome += start;
+        start++;
+    }
+    return outcome
 };
 
 // Do not edit below this line
