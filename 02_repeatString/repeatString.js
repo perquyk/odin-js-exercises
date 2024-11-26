c0effeb9b5b03f927382b7c70ae3246ea62307af
Plopkoekk <41813101+perquyk@users.noreply.github.com>
2024-11-26 12:26:34 +0100

    did first 3 excersices

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let counter = 0;
+    if(num<0){return 'ERROR'}
+    let newstring = '';
+    while(counter<num){
+        newstring += string;
+        counter++;
+    }
+    return newstring;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringArray = string.split("");
+    return stringArray.reverse().join("");
 };
 
 // Do not edit below this line
