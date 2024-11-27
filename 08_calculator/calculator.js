9eeaa8bd75b36a7de12f7e3d3183821aee1ed224
Plopkoekk <41813101+perquyk@users.noreply.github.com>
2024-11-27 21:43:47 +0100

    did ex.08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(x, y) {
+ return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let ar = array;
+  let sumOf = 0;
+  if(ar.length > 0){
+    ar.forEach((item) => {
+      sumOf += item;
+    })
+  }
+  else{
+    return 0;
+  }
+  return sumOf;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let ar = array;
+  let productOf = 1;
+  ar.forEach((item) => {
+    productOf = productOf * item
+  })
+  return productOf;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let powerOf = 1;
+	for(let i = 0; i<y ; i++){
+    powerOf = powerOf * x;
+  }
+  return powerOf;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let factorialOf = 1;
+  for(let i = x; i>0; i--){
+    factorialOf = factorialOf * i;
+  }
+  return factorialOf;
 };
 
 // Do not edit below this line
