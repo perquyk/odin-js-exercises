b21eec3e431554863188f13fbd1351de6a51be12
Plopkoekk <41813101+perquyk@users.noreply.github.com>
2024-11-27 22:30:46 +0100

    did excercise 10
    I skipped 9 as I've been spoiled by accident.
    I'll pick it up another day to have the full
    effect of the excercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if(x<0){return "OOPS"};
+    const output = [0,1];
+    
+    for(let i = 0; i<x ; i++){
+        output.push(output[i+1]+output[i])
+    }
+    return  output[x];
 };
 
 // Do not edit below this line
